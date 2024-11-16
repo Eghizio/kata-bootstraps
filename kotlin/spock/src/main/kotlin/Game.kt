6ad6e6c66d26aa2d0c96c85f065d8c6d405aa1a3
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2024-11-16 22:11:43 +0100

    Failing neighbours count test

--- kotlin/spock/src/main/kotlin/Game.kt
+++ kotlin/spock/src/main/kotlin/Game.kt
@@ -1,3 +1,17 @@
 class Game(positions: List<Position>) {
     private val cells: HashSet<Position> = positions.toHashSet()
+
+    fun getNeighboursCountForCell(cell: Position): Int {
+        return -1
+    }
 }
+
+//interface GameCommand {
+//    fun invoke(game: Game): Game
+//}
+//
+//data class GetNeighboursCommand(val cell: Position, val cells: Set<Position>) : GameCommand {
+//    override fun invoke(game: Game): Game {
+//        return game
+//    }
+//}
--- kotlin/spock/src/test/groovy/GameTest.groovy
+++ kotlin/spock/src/test/groovy/GameTest.groovy
@@ -18,4 +18,25 @@ class GameTest extends Specification {
         expect: "game contains appropriate amount of cells"
         game.cells.size() == 3
     }
+
+    def "should get count of neighbours for cell"() {
+        given: "we have cells"
+        Position neighbour1 = new Position(0, 0)
+        Position neighbour2 = new Position(1, 0)
+        Position loneCell = new Position(42, 1337)
+
+        and:
+        List<Position> cells = [neighbour1, neighbour2, loneCell]
+        Game game = new Game(cells)
+
+        then:
+        def count1 = game.getNeighboursCountForCell(neighbour1)
+        def count2 = game.getNeighboursCountForCell(neighbour2)
+        def count3 = game.getNeighboursCountForCell(loneCell)
+
+        expect:
+        count1 == 1
+        count2 == 1
+        count3 == 0
+    }
 }
